9e04c9b226a839323366c87303dcb27477431f93
IceDarron <darron_r@163.com>
2019-03-12 23:28:36 +0800

    Initial commit

---
+++ src/main/java/com/icedarron/kafka/controller/KafkaProducerController.java
@@ -0,0 +1,24 @@
+package com.icedarron.kafka.controller;
+
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.kafka.core.KafkaTemplate;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试kafka生产者
+ */
+@RestController
+@RequestMapping("kafka")
+public class KafkaProducerController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @RequestMapping("send")
+    public String send(String msg) {
+        kafkaTemplate.send("test_topic", msg);
+        return "success";
+    }
+
+}
